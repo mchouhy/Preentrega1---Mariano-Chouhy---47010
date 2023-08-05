f25a599c501d57f1c11a5042f24897dcf0dcda86
Mariano Chouhy <marianochouhy@gmail.com>
2023-08-05 15:13:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,132 @@
+// VARIABLES GLOBALES //
+
+let user;
+let password;
+let conversionUnit;
+let conversionResult = 0;
+
+// FUNCIONES //
+
+function userRegistration() {
+    user = prompt("Register a user name");
+    while (user.trim() === "") {
+        errorAlert('user');
+        user = prompt("Register a user name");
+    }
+    if (user !== "") {
+        console.log(`Hello ${user}!`);
+        alert(`Hello ${user}!`);
+    }
+    }
+
+function errorAlert(param1) {
+        alert(`Error: You have to register a valid ${param1}.`);
+    }
+
+function passwordRegistration() {
+    password = prompt("Register a password");
+    while (password.trim() === "") {
+        errorAlert('password');
+        password = prompt("Register a password");
+    }
+    if (password !== "") {
+        console.log(`Congratulations! You are now registered. Remember that your password is: ${password}.`);
+        alert(`Congratulations! You are now registered. Remember that your password is: ${password}. Now click acept and start converting!`);
+    }
+    }
+
+function getUnit() {
+    conversionUnit = prompt("Choose what CSS length units you want to convert. Type:\npx to rem\nrem to px\npx to cm\ncm to px");
+    while (conversionUnit.trim() === "") {
+        errorAlert2('units');
+        conversionUnit = prompt("Choose what CSS length units you want to convert. Type:\npx to rem\nrem to px\npx to cm\ncm to px");
+    }
+    if (conversionUnit === "px to rem" || conversionUnit === "rem to px" || conversionUnit === "px to cm" || conversionUnit === "cm to px") {
+        conversionNumber = parseFloat(prompt("Enter the number you want to convert")); {
+            while (isNaN (conversionNumber)) {
+            errorAlert2 ('number');
+            conversionNumber = parseFloat(prompt("Enter the number you want to convert"));
+        }
+    }
+    
+}}
+
+function errorAlert2(param2) {
+    alert(`Error: ${user} you have to enter the lenght ${param2} you want to convert as indicated. Try again.`);
+}
+
+function conversionCalculator(){
+    switch (conversionUnit) {
+        case "px to rem":
+            conversionResult = conversionNumber / 16;
+            console.log(`${conversionNumber} px = ${conversionResult} rem.`);
+            alert(`${conversionNumber} px = ${conversionResult} rem.`);
+            break
+        case "rem to px":
+            conversionResult = conversionNumber * 16;
+            console.log(`${conversionNumber} rem = ${conversionResult} px.`);
+            alert(`${conversionNumber} rem = ${conversionResult} px.`);
+            break
+        case "px to cm":
+            conversionResult = conversionNumber / 37.7952755906;
+            console.log(`${conversionNumber} px = ${conversionResult} cm.`);
+            alert(`${conversionNumber} px = ${conversionResult} cm.`);
+            break
+        case "cm to px":
+            conversionResult = conversionNumber * 37.7952755906; 
+            console.log(`${conversionNumber} cm = ${conversionResult} px.`);
+            alert(`${conversionNumber} cm = ${conversionResult} px.`);
+            break
+        default:
+            alert(`Error: ${user} you have to enter the lenght units you want to convert as indicated. Try again.`);
+            getUnit();
+            conversionCalculator();
+    }
+}
+
+//MENSAJE DE BIENVENIDA//
+
+alert("Welcome to Pixel Converter. Your CSS length unit calculator. Disclaimer: for the moment we only work with Pixels, REMs and centimeters.");
+
+//REGISTRO DE USUARIO//
+
+userRegistration();
+
+//REGISTRO DE CONTRASEÑA//
+
+passwordRegistration();
+
+//SOLICITAR DE MEDIDA DE CSS Y VALOR A CONVERTIR//
+
+getUnit();
+
+//CÁLCULO DEL RESULTADO//
+
+conversionResult = 0;
+
+conversionCalculator();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

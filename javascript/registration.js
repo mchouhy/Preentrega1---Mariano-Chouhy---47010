925ffd3f6321e9421247582eaa805b09020e9e95
Mariano Chouhy <marianochouhy@gmail.com>
2023-09-04 14:35:36 +0200

    link error

--- javascript/registration.js
+++ javascript/registration.js
@@ -6,26 +6,17 @@ let passwordInput = document.getElementById('passwordInput');
 
 let loginBtn = document.getElementById('loginBtn');
 
-let loginForm = document.getElementById('login-form');
+let registerForm = document.getElementById('register-form');
 
-//FUNCION PARA MENSAJE EVITAR REGISTRAR EN CASO DE STRINGS VACÍOS
-
-function changeAction() {
-    return loginForm.action = "#header-logo";
-}
-
-//EVENTO SOBRE EL BOTON DE REGISTER Y FUNCIÓN QUE ALMACENA USUARIO Y CONTRASEÑA EN EL LOCAL STORAGE
+//EVENTO SOBRE EL BOTON DE SUBMIT Y FUNCIÓN QUE ALMACENA USUARIO Y CONTRASEÑA EN EL LOCAL STORAGE
 
 loginBtn.onclick = newUser = () =>{
-    user = userInput.value;
-    
-    password = passwordInput.value;
 
-    localStorage.setItem('userName', user);
+    let user = userInput.value;
     
-    localStorage.setItem('userPassword', password);
+    let password = passwordInput.value;
 
-// CONDICIONAL CON MENSAJE TOAST DE ERROR EN CASO DE NO REGISTRAR UN USUARIO Y/O CONTRASEÑA
+// CONDICIONAL CON MENSAJE TOAST DE ERROR EN CASO DE NO REGISTRAR UN USUARIO Y/O CONTRASEÑA. ALMACENAMIENTO EN LOCAL STORAGE SI SE INGRESA USUARIO Y CONTRASEÑA.
 
     if (user === "" && password === "") {
         Toastify({
@@ -42,7 +33,6 @@ loginBtn.onclick = newUser = () =>{
             },
             onClick: function () { } // Callback after click
         }).showToast();
-        changeAction();
         
     } else if (user === "") {
         Toastify({
@@ -59,7 +49,6 @@ loginBtn.onclick = newUser = () =>{
             },
             onClick: function () { } // Callback after click
         }).showToast();
-        changeAction();
 
     } else if (password === "") {
         Toastify({
@@ -76,8 +65,25 @@ loginBtn.onclick = newUser = () =>{
             },
             onClick: function () { } // Callback after click
         }).showToast();
-        changeAction();
+
     } else {
-        newUser();
-    }
+        Toastify({
+            text: "Registration succesful. Now click on Login.",
+            duration: 5000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "linear-gradient(to right, #03c04a, #03ac13)",
+            },
+            onClick: function () { } // Callback after click
+        }).showToast();
+    
+        localStorage.setItem('userName', user);
+        
+        localStorage.setItem('userPassword', password);
+    }  
 }

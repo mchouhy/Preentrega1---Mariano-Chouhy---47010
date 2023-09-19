a6e7d2de3cb238bf4142b11fb381cf1dd3a0ea50
Mariano Chouhy <marianochouhy@gmail.com>
2023-09-19 18:06:53 +0200

    se agrega fetch y validación de usuarios de json local

--- javascript/login.js
+++ javascript/login.js
@@ -26,88 +26,101 @@ loginBtn.onclick = validateUser = () => {
 
     checkUserInfo = JSON.parse(localStorage.getItem('userInfo'));
 
+//FUNCIÓN PARA TRAER LOS USUARIOS GUARDADOS EN EL ARCHIVO LOCAL DE 
+
+    fetch("../json/usersDB.json")
+        .then((response) => response.json())
+        .then(({ userDB }) => {
+
+            const usuario = userDB.find((user) => user.username === userData && user.password === passwordData);
+
 //CONDICIONAL PARA VALIDAR SI EL USUARIO Y CONTRASEÑA INGRESADO ESTÁ REGISTRADO
+            
+            if ((userData === checkUserInfo.user && passwordData === checkUserInfo.password) || usuario) {
+                Toastify({
+                    text: "Login succesful",
+                    duration: 5000,
+                    destination: "https://github.com/apvarun/toastify-js",
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "center", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #03c04a, #03ac13)",
+                    },
+                    onClick: function () { } // Callback after click
+                }).showToast();
 
-    if (userData === checkUserInfo.user && passwordData === checkUserInfo.password) {
-        Toastify({
-            text: "Login succesful",
-            duration: 5000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "linear-gradient(to right, #03c04a, #03ac13)",
-            },
-            onClick: function () {} // Callback after click
-        }).showToast();
+                setTimeout(() => {
+                    loginSuccesfull()
+                }, 2000);
+            } else if (userData === "" && passwordData === "") {
+                Toastify({
+                    text: "Enter your username and password",
+                    duration: 3000,
+                    destination: "https://github.com/apvarun/toastify-js",
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "center", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #f94f55, #f94f5b)",
+                    },
+                    onClick: function () { } // Callback after click
+                }).showToast();
 
-        setTimeout(() => {loginSuccesfull()
-        }, 2000);
-    } else if (userData === "" && passwordData === "") {
-        Toastify({
-            text: "Enter your username and password",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "linear-gradient(to right, #f94f55, #f94f5b)",
-            },
-            onClick: function () { } // Callback after click
-        }).showToast();
+            } else if (userData === "") {
+                Toastify({
+                    text: "Enter your username",
+                    duration: 3000,
+                    destination: "https://github.com/apvarun/toastify-js",
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "center", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #f94f55, #f94f5b)",
+                    },
+                    onClick: function () { } // Callback after click
+                }).showToast();
 
-    } else if (userData === "") {
-        Toastify({
-            text: "Enter your username",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "linear-gradient(to right, #f94f55, #f94f5b)",
-            },
-            onClick: function () { } // Callback after click
-        }).showToast();
+            } else if (passwordData === "") {
+                Toastify({
+                    text: "Enter your password",
+                    duration: 3000,
+                    destination: "https://github.com/apvarun/toastify-js",
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "center", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #f94f55, #f94f5b)",
+                    },
+                    onClick: function () { } // Callback after click
+                }).showToast();
+            }
+            else {
+                Toastify({
+                    text: "Wrong user or password. Try again.",
+                    duration: 3000,
+                    destination: "https://github.com/apvarun/toastify-js",
+                    newWindow: true,
+                    close: true,
+                    gravity: "top", // `top` or `bottom`
+                    position: "center", // `left`, `center` or `right`
+                    stopOnFocus: true, // Prevents dismissing of toast on hover
+                    style: {
+                        background: "linear-gradient(to right, #f94f55, #f94f5b)",
+                    },
+                    onClick: function () { } // Callback after click
+                }).showToast();
+            }
+        });
+};
 
-    } else if (passwordData === "") {
-        Toastify({
-            text: "Enter your password",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "linear-gradient(to right, #f94f55, #f94f5b)",
-            },
-            onClick: function () { } // Callback after click
-        }).showToast();
-    }
-    else {
-        Toastify({
-            text: "Wrong user or password. Try again.",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-                background: "linear-gradient(to right, #f94f55, #f94f5b)",
-            },
-            onClick: function () { } // Callback after click
-        }).showToast();
-    }
-}   
+
+
